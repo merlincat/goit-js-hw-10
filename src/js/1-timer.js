f5a01e685fb1dc5af3b14fc3c49192588fc923a4
Nata <40355692+merlincat@users.noreply.github.com>
2024-01-01 21:02:45 +0200

    dis btn at start

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -11,6 +11,9 @@ const hoursElement = document.querySelector('[data-hours]');
 const minutesElement = document.querySelector('[data-minutes]');
 const secondsElement = document.querySelector('[data-seconds]');
 
+document.addEventListener('DOMContentLoaded', () => {
+  startButton.disabled = true;
+});
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
